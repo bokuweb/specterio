8782e09ec4d535cd92c1c1137b68bedd79a9c27b
bokuweb <bokuweb12@gmail.com>
2016-07-29 15:20:30 +0900

    refactor

--- helper/client.js
+++ helper/client.js
@@ -3,65 +3,27 @@ const log = require('./log');
 exports.initialize = (client, url) => (
   new Promise((resolve) => {
     client
-      .goto(url)
-      .inject('css', 'init.css')
-      .viewport(1280, 768)
-      .wait('body')
-      .scrollTo(999999, 0)
-      .scrollTo(0, 0)
-      .then(res => {
-        log.debug('initialized');
-        log.debug(`url = ${url}`);
-        resolve(res);
-      })
+      .url(url)
+      .waitForExist('body', 60000)
+      .then(resolve)
       .catch(err => log.error(err));
   })
 );
 
-exports.getDimmensions = client => (
+exports.capture = (client) => (
   new Promise((resolve) => {
     client
-      .evaluate(() => {
-        const body = document.querySelector('body');
-        return {
-          height: body.scrollHeight + window.outerHeight - window.innerHeight,
-          width: body.scrollWidth,
-        };
-      })
-      .then(res => {
-        log.debug('get dimensions');
-        log.debug(res);
-        resolve(res);
-      })
+      .saveScreenshot('./screenshot/snapshot.png')
+      .then(resolve)
       .catch(err => log.error(err));
   })
 );
 
-exports.getHrefs = client => (
+exports.getLinks = (client) => (
   new Promise((resolve) => {
     client
-      .evaluate(() => {
-        const links = [].slice.call(document.querySelectorAll('a'));
-        return links.map(link => link.href);
-      })
-      .then(res => {
-        log.debug('get Hrefs');
-        log.debug(`href number = ${res.length}`);
-        resolve(res);
-      })
-      .catch(err => log.error(err));
-  })
-);
-
-exports.capture = (client, { width, height }, path) => (
-  new Promise((resolve) => {
-    client
-      .viewport(width, height)
-      .screenshot(path)
-      .then(res => {
-        log.debug('captured');
-        resolve(res);
-      })
+      .getAttribute('a', 'href')
+      .then(resolve)
       .catch(err => log.error(err));
   })
 );
--- index.js
+++ index.js
@@ -6,29 +6,16 @@ const selenium = require('selenium-standalone');
 const log = require('./helper/log');
 const cli = require('./helper/cli');
 const client = webdriverio.remote({ desiredCapabilities: { browserName: 'firefox' } });
-const capturedPath = [];
-
-const initialize = (client, url) => (
-  new Promise((resolve) => {
-    client
-      .url(url)
-      .waitForExist('body', 60000)
-      .saveScreenshot('./screenshot/snapshot.png')
-      .getAttribute('a', 'href')
-      .then(hrefs => {
-        log.debug('initialized');
-        log.debug(`url = ${url}`);
-        resolve(hrefs);
-      })
-      .catch(err => log.error(err));
-  })
-);
+const { initialize, capture, getLinks } = require('./helper/client');
 
 selenium.start(() => {
+  const capturedPath = [];
   const run = co.wrap(function * (url, depth, index) {
     if (depth >= 2) return;
     log.debug(`depth = ${depth}`);
-    const hrefs = yield initialize(client, url);
+    yield initialize(client, url);
+    yield capture(client);
+    const hrefs = yield getLinks(client);
     capturedPath.push(url);
     const filterdHrefs = hrefs
             .filter((href, i, self) => (
@@ -42,57 +29,12 @@ selenium.start(() => {
       yield run(href, depth + 1, i);
     }
   });
-  client.init().then(() => {
-    run(cli.flags.url, 0, 0).then(() => {
-      log.info('comlete');
+  client.init()
+    .then(() => {
+      run(cli.flags.url, 0, 0)
+        .then(() => {
+          log.info('complete');
+          client.end().then(() => process.exit());
+        });
     });
-  })
-
 });
-
-
-/*
-const log = require('./helper/log');
-const Nightmare = require('nightmare');
- const co = require('co');
-const cli = require('./helper/cli');
-const {
-  initialize,
-  getDimmensions,
-  getHrefs,
-  capture,
-} = require('./helper/client');
-
-const options = {
-  show: false,
-  width: 1280,
-  height: 768,
-};
-const nightmare = new Nightmare(options);
-const path = 'temp';
-const capturedPath = [];
-
-const run = co.wrap(function * (url, depth, index) {
-  if (depth >= 2) return;
-  log.debug(`depth = ${depth}`);
-  yield initialize(nightmare, url);
-  const dimensions = yield getDimmensions(nightmare);
-  const hrefs = yield getHrefs(nightmare);
-  yield capture(nightmare, dimensions, `${path}/${depth}-${index}.png`);
-  capturedPath.push(url);
-  const filterdHrefs = hrefs
-          .filter((href, i, self) => (
-            new RegExp(`^${cli.flags.url}|^(?!http)`).test(href) ||
-            self.indexOf(href) === i ||
-            capturedPath.indexOf(href) === -1
-          ));
-  for (const [i, href] of filterdHrefs.entries()) {
-    yield run(href, depth + 1, i);
-  }
-  if (depth === 0) yield nightmare.end();
-});
-
-run(cli.flags.url, 0, 0).then(() => {
-  log.info('comlete');
-});
-*/
