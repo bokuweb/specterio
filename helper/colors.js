f6ce3fc050df418673308a4d5e4f4ca6261829f2
bokuweb <bokuweb12@gmail.com>
2016-07-31 07:49:01 +0900

    Add console color vars

---
+++ helper/colors.js
@@ -0,0 +1,9 @@
+exports.black = '\u001b[30m';
+exports.red = '\u001b[31m';
+exports.green = '\u001b[32m';
+exports.yellow = '\u001b[33m';
+exports.blue = '\u001b[34m';
+exports.magenta = '\u001b[35m';
+exports.cyan = '\u001b[36m';
+exports.white = '\u001b[37m';
+exports.reset = '\u001b[0m';
